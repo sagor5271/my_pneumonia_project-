577d7d43f0f3d59a0c3c179f3f901732dc32b538
sagor5271 <sagor.bme.iu@gmail.com>
2025-10-03 10:34:28 +0600

    Update fdconv.py

--- custom_modules/fdconv.py
+++ custom_modules/fdconv.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from numpy.linalg import matrix_rank
 from torch.utils.checkpoint import checkpoint
 
-from mmcv.cnn import CONV_LAYERS
+ #from mmcv.cnn import CONV_LAYERS
 from torch import Tensor
 import torch.nn.functional as F
 import math
